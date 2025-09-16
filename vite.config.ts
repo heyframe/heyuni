576d8e6721cdb362307d201ee97c36b02a55103c
gxiaosong <lyricsong47@gmail.com>
2025-09-16 12:01:46 +0000

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import {defineConfig, loadEnv} from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import * as path from "node:path";
+import colors from 'picocolors';
 
-export default ({command, mode}) => {
+export default defineConfig(({command, mode}) => {
+  const isProd = command === 'build';
+  const isDev = !isProd;
+  const base = isProd ? '/bundles/uni/uni' : undefined;
+
+  const {UNI_PLATFORM} = process.env
+  console.log(colors.yellow(`UNI_PLATFORM: ${UNI_PLATFORM}`))
 
   const env = loadEnv(mode, path.resolve(process.cwd()))
-  return defineConfig({
+  const {VITE_APP_PORT,} = env
+  return {
+    base,
     plugins: [
       uni(),
     ],
@@ -14,10 +23,14 @@ export default ({command, mode}) => {
         '@': path.join(process.cwd(), './src'),
       },
     },
+    server: {
+      host: process.env.HOST ? process.env.HOST : 'localhost',
+      port: Number(VITE_APP_PORT) || 9000,
+    },
     build: {
       sourcemap: false,
       target: 'es6',
       minify: mode === 'development' ? false : 'esbuild',
-    },
-  });
-}
+    }
+  }
+});
